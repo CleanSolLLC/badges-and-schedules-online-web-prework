ddc3d48f3f8e7328a6d3e3389e108843a43644fa
Mark May <mamark90@gmail.com>
2019-03-26 19:38:43 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -35,8 +35,8 @@ end
 def printer(attendees)
   badges_and_room_assignments = []
 
-  badges_and_room_assignments  = batch_badge_creator(participants)
-  badges_and_room_assignments  = assign_rooms(participants)
+  badges_and_room_assignments  = batch_badge_creator(attendees)
+  badges_and_room_assignments  = assign_rooms(attendees)
 
   badges_and_room_assignments.each {|guest_info|
     puts guest_info
