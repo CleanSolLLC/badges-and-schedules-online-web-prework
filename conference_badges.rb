d90342749b4ca8fb79fdac5dc66c3b7056a7e541
Mark May <mamark90@gmail.com>
2019-03-26 20:16:30 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -33,13 +33,13 @@ end
 
 
 def printer(attendees)
-  printer_array = []
-  printer_array  = batch_badge_creator(attendees)
-  printer_array  << assign_rooms(attendees)
+   = []
+  badges_and_room_assignments  = batch_badge_creator(attendees)
+    badges_and_room_assignments  << assign_rooms(attendees)
 
-    printer_array.each {|guest_info|
+    badges_and_room_assignments.each {|guest_info|
     #  badges_and_room_assignments += guest_info
-      puts   guest_info/n
+      puts badges_and_room_assignments/n
   }
 
 end
